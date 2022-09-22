cb9fe732325108ad7a7202bf08e8e3b6f582aedf
Mojca Miklavec <mojca.miklavec@dewesoft.com>
2022-09-22 12:47:25 +0200

    VRML issue

---
+++ box.py
@@ -0,0 +1,7 @@
+import cadquery as cq
+
+box = cq.Workplane().box(2.0, 4.0, 1.0)
+assy = cq.Assembly(box, loc=cq.Location((0,0,0)), color=cq.Color("black"))
+
+cq.exporters.export(box, 'box-ok.wrl', cq.exporters.ExportTypes.VRML)
+assy.save('box-has-back-face-culling-issues.wrl', exportType='VRML')
