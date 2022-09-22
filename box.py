27e2a8e25703545edf8cd3f16071201e324f69c2
Mojca Miklavec <mojca.miklavec@dewesoft.com>
2022-09-22 19:19:41 +0200

    Export assembly to .step

--- box.py
+++ box.py
@@ -5,3 +5,4 @@ assy = cq.Assembly(box, loc=cq.Location((0,0,0)), color=cq.Color("black"))
 
 cq.exporters.export(box, 'box-ok.wrl', cq.exporters.ExportTypes.VRML)
 assy.save('box-has-back-face-culling-issues.wrl', exportType='VRML')
+assy.save('box.step')
